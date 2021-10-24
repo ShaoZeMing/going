0076bfbbc1282ae4c85a3088c08b3fb8273b8574
shaozeming <szm19920426@gmail.com>
2021-10-24 23:39:00 +0800

    添加配置文件目录和配置结构定义

---
+++ config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	App   app
+	Mysql mysql
+	Log   log
+	Extra extra
+}
+
+type app struct {
+	AppName    string `json:"app_name"`    //应用名称
+	AppEnv     string `json:"app_env"`     //应用环境
+	RunMode    string `json:"run_mode"`    //运行模式
+	HTTPListen string `json:"http_listen"` //应用监听HTTP服务=IP:端口
+	GrpcListen string `json:"grpc_listen"` //应用监听grpc= IP:端口
+}
+
+type mysql struct {
+	Dsn             string `json:"dsn"`
+	MaxIdleConn     int    `json:"maxIdleConn"`
+	MaxOpenConn     int    `json:"maxOpenConn"`
+	ConnMaxLifetime int    `json:"connMaxLifetime"`
+}
+
+type extra struct {
+	DingRobotKey string `json:"ding_robot_key"`
+}
+
+type log struct {
+	Channel string `json:"channel"`  //日志记录渠道
+	DirPath string `json:"dir_path"` //日志记录目录地址，绝对路径
+	Days    int    `json:"days"`     //日志记录天数
+}
+
+func ConfInit() *Config {
+
+	var conf *Config
+	viper.SetConfigName("app")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+		return nil
+	}
+	err = viper.Unmarshal(conf)
+	if err != nil {
+		panic(err)
+		return nil
+	}
+	return conf
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/ShaoZeMing/going
 go 1.16
 
 require (
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
+	github.com/coreos/etcd v3.3.10+incompatible // indirect
+	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.7.3
+	github.com/spf13/viper v1.9.0 // indirect
+	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
 	go.opentelemetry.io/otel v1.0.1 // indirect
 	go.opentelemetry.io/otel/trace v1.0.1 // indirect
 	gorm.io/driver/mysql v1.1.2 // indirect
